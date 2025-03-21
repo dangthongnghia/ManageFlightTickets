2174b56dbca41647d1b4e3c0407785a4890d49c3
Đặng Thông Nghĩa <141739914+dangthongnghia@users.noreply.github.com>
2025-03-21 13:24:18 +0700

    CreateFolder

---
+++ ManageFlightTickets/Models/Account.cs
@@ -0,0 +1,23 @@
+﻿namespace ManageFlightTickets.Models
+{
+    public class Account
+    {
+       
+        public string IdAccount { get; set; } 
+        public string Username { get; set; }
+        public string Password { get; set; }
+        
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string IdentityCard { get; set; }
+        
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string FullName { get; set; }
+        public string Status { get; set; }
+        public string Role { get; set; }
+        public bool IsRole { get; set; }
+
+    }
+}
